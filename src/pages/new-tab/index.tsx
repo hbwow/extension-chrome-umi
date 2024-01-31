5c875c2c5dedd4012c5d88f277bd6c34b82cd82a
hbwow <lllengkaixin@gmail.com>
2024-01-31 17:25:05 +0800

    feat: 🎸 交互优化

--- src/pages/new-tab/index.tsx
+++ src/pages/new-tab/index.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import cx from 'classnames';
 
 import Layouts from '@/components/layouts';
 import { Button, Form, Input, Dropdown } from 'antd';
-import { RiGoogleFill } from 'react-icons/ri';
 
+import Mousetrap from 'mousetrap';
 import useStore from '@/pages/new-tab/store';
 import { useRegularRefreshDate } from '@/utils/hooks';
 import { greetings } from '@/utils/common';
 import { SEARCH_ENGINE_ICONS } from '@/utils/constants';
 
 const NewTab = () => {
+  const defaultUpDownSelected = { curLength: -1, data: {} };
+  const [upDownSelected, setUpDownSelected] = useState({
+    ...defaultUpDownSelected,
+  });
+
   const {
     bookmarkSearchResult,
     historySearchResult,
@@ -27,14 +32,6 @@ const NewTab = () => {
 
   const { date } = useRegularRefreshDate();
 
-  useEffect(() => {
-    updateBookmarkTreeNode();
-    updateHistoryItem();
-
-    listenerStorageForShowName();
-    listenerStorageForSearchEngine();
-  }, []);
-
   const handleFinish = (values: any) => {
     const q = values.search ?? '今日天气？';
     window.location.href = storageForSearchEngine + q;
@@ -43,8 +40,68 @@ const NewTab = () => {
   const handleValuesChange = (changedValues: any, allValues: any) => {
     updateBookmarkSearchResult(changedValues.search);
     updateHistorySearchResult(changedValues.search);
+
+    setUpDownSelected({ ...defaultUpDownSelected });
   };
 
+  const handleInputKeydown = (e) => {
+    if (e.keyCode !== 38 && e.keyCode !== 40 && e.keyCode !== 13) {
+      return;
+    }
+
+    // Enter 逻辑
+    if (e.keyCode === 13) {
+      if (upDownSelected.curLength === -1) {
+        return;
+      } else {
+        e.preventDefault();
+        window.location.href = upDownSelected.data.url;
+
+        return;
+      }
+    }
+
+    if (e.keyCode === 38 || e.keyCode === 40) {
+      e.preventDefault();
+    }
+
+    const allSearchResult = [...bookmarkSearchResult, ...historySearchResult];
+    let nextCurLength = 0;
+
+    switch (e.keyCode) {
+      case 38: {
+        // Handle up arrow key
+        nextCurLength =
+          upDownSelected.curLength - 1 <= -1
+            ? allSearchResult.length - 1
+            : upDownSelected.curLength - 1;
+        break;
+      }
+      case 40: {
+        // Handle down arrow key
+        nextCurLength =
+          upDownSelected.curLength + 1 > allSearchResult.length - 1
+            ? 0
+            : upDownSelected.curLength + 1;
+        break;
+      }
+    }
+
+    setUpDownSelected({
+      curLength: nextCurLength,
+      data: allSearchResult[nextCurLength],
+    });
+  };
+
+  useEffect(() => {
+    updateBookmarkTreeNode();
+    updateHistoryItem();
+
+    listenerStorageForShowName();
+    listenerStorageForSearchEngine();
+  }, []);
+
+  // 是否打开
   const isOpen =
     bookmarkSearchResult.length > 0 || historySearchResult.length > 0;
 
@@ -52,7 +109,7 @@ const NewTab = () => {
 
   return (
     <Layouts>
-      <div className="flex-1 flex flex-col items-center pt-180">
+      <div className="flex-1 flex flex-col items-center pt-120">
         <div>
           <div className="pl-12 text-green-600">{greetings(date)}</div>
           <h1 className="w-600 text-48 mb-24 px-12">
@@ -82,7 +139,10 @@ const NewTab = () => {
                           return (
                             <li key={id}>
                               <Button
-                                className="flex mt-4"
+                                className={cx('flex mt-4', {
+                                  'lll-btn-active':
+                                    upDownSelected.data.url === url,
+                                })}
                                 type="text"
                                 href={url}
                               >
@@ -105,7 +165,10 @@ const NewTab = () => {
                           return (
                             <li key={id}>
                               <Button
-                                className="flex mt-4"
+                                className={cx('flex mt-4', {
+                                  'lll-btn-active':
+                                    upDownSelected.data.url === url,
+                                })}
                                 type="text"
                                 href={url}
                               >
@@ -124,6 +187,7 @@ const NewTab = () => {
             <div className="relative">
               <Form.Item name="search">
                 <Input
+                  onKeyDown={handleInputKeydown}
                   className={cx(
                     'w-600 h-60 pl-24 pr-64 bg-bg-600 hover:bg-bg-600 focus:bg-bg-600 border-2 border-white text-20 transition-all duration-300',
                     {
